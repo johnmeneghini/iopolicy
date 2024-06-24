71e9ace8686afb08fbf121991e1ccca4a3e2490b
John Meneghini <jmeneghi@redhat.com>
2024-06-24 10:15:09 -0400

    Add support for bench-fio to start_fio.sh
    Signed-off-by: John Meneghini <jmeneghi@redhat.com>

--- start_fio.sh
+++ start_fio.sh
@@ -4,9 +4,8 @@
 
 if [ $# -lt 6 -o $# -gt 6 ]; then
 	echo "usage: $0 <disk> <test> <block_size> <runtime> <numjobs> <iodepth>"
-	echo " e.g.: $0 nvme16n1 randread 64k 18800 40 64"
+	echo " e.g.: $0 nvme9n2 bench-fio 4k 60 \"8 16 32 64\" \"16 32 64 128\""
 	echo " e.g.: $0 nvme5n2 randrw 512k 28800 64 127"
-	echo " e.g.: $0 nvme5n2 write 4k 28800 64 32"
 	exit 1
 fi
 
@@ -24,30 +23,55 @@ if ! [ -b /dev/$disk ]; then
   exit 1
 fi
 
+ENGINE="fio"
 TESTARG=""
+TESTARG2=""
+TESTARG3="$TEST"
+DISK=""
+OUTPUT=""
 
 case "$TEST" in
 	"read")
+		DISK=" --filename=/dev/${disk}"
 		TESTARG="--rw=randread  --eta-newline=1 --readonly"
+        TESTARG2=" --ioengine=libaio --direct=1 --size=400G ${TESTARG} --bs=${BS} --iodepth=${iodepth} --numjobs=${numjobs} --runtime=${runtime} --group_reporting --name=${disk}_${BS}_${TEST}_test --output=/dev/null"
 		;;
 	"write")
+		DISK=" --filename=/dev/${disk}"
 		TESTARG="--rw=write"
+        TESTARG2=" --ioengine=libaio --direct=1 --size=400G ${TESTARG} --bs=${BS} --iodepth=${iodepth} --numjobs=${numjobs} --runtime=${runtime} --group_reporting --name=${disk}_${BS}_${TEST}_test --output=/dev/null"
 		;;
 	"randread")
+		DISK=" --filename=/dev/${disk}"
 		TESTARG="--rw=randread --refill_buffers --norandommap --randrepeat=0 --readonly"
+        TESTARG2=" --ioengine=libaio --direct=1 --size=400G ${TESTARG} --bs=${BS} --iodepth=${iodepth} --numjobs=${numjobs} --runtime=${runtime} --group_reporting --name=${disk}_${BS}_${TEST}_test --output=/dev/null"
 		;;
 	"randwrite")
+		DISK=" --filename=/dev/${disk}"
 		TESTARG="--rw=randwrite --refill_buffers --norandommap --randrepeat=0"
+        TESTARG2=" --ioengine=libaio --direct=1 --size=400G ${TESTARG} --bs=${BS} --iodepth=${iodepth} --numjobs=${numjobs} --runtime=${runtime} --group_reporting --name=${disk}_${BS}_${TEST}_test --output=/dev/null"
 		;;
 	"rw,randwrite")
+		DISK=" --filename=/dev/${disk}"
 		TESTARG="--rw=rw,randwrite --refill_buffers --norandommap --randrepeat=0"
+        TESTARG2=" --ioengine=libaio --direct=1 --size=400G ${TESTARG} --bs=${BS} --iodepth=${iodepth} --numjobs=${numjobs} --runtime=${runtime} --group_reporting --name=${disk}_${BS}_${TEST}_test --output=/dev/null"
 		;;
 	"randrw")
+		DISK=" --filename=/dev/${disk}"
 		TESTARG="--rw=randrw --refill_buffers --norandommap --randrepeat=0 --rwmixread=50"
+        TESTARG2=" --ioengine=libaio --direct=1 --size=400G ${TESTARG} --bs=${BS} --iodepth=${iodepth} --numjobs=${numjobs} --runtime=${runtime} --group_reporting --name=${disk}_${BS}_${TEST}_test --output=/dev/null"
+		;;
+	"bench-fio")
+		ENGINE="bench-fio"
+		OUTPUT="/run/benchfio"
+		DISK=" -d /dev/$disk"
+		TESTARG="-m read randread write randwrite"
+		TESTARG2=" -t device -o $OUTPUT  -b ${BS} --iodepth ${iodepth} --numjobs ${numjobs} --runtime ${runtime} ${TESTARG} --direct=1 --destructive"
+		TESTARG3="read randread write randwrite"
 		;;
 	*)
 		echo "Error: \"$TEST\" is invalid."
-		echo "usage: $0 <disk> <read|write|randread|randwrite|rw,randwrite|randrw> <block_size> <runtime> <numjobs> <iodepth>"
+		echo "usage: $0 <disk> <read|write|randread|randwrite|rw,randwrite|randrw|bench-fio> <block_size> <runtime> <numjobs> <iodepth>"
 		exit 1
 		;;
 esac
@@ -61,10 +85,14 @@ echo "" >> system.cfg
 lscpu >> system.cfg
 echo "" >> system.cfg
 
-echo "fio ioengine=libaio direct=1 rw=${TEST} bs=${BS} iodepth=${iodepth} numjobs=${numjobs}" > fio.txt
-echo "fio --filename=/dev/${disk} --ioengine=libaio --direct=1 --size=400G ${TESTARG} --bs=${BS} --iodepth=${iodepth} --numjobs=${numjobs} --runtime=${runtime} --group_reporting --name=${disk}_${BS}_${TEST}_test --output=/dev/null" > fio_command.txt
+echo "$ENGINE libaio direct ${TESTARG3} ${BS} iodepth ${iodepth} numjobs ${numjobs}" > fio.txt
+echo "$ENGINE $DISK $TESTARG2" > fio_command.txt
 cat fio_command.txt
 echo ""
 
-fio --filename=/dev/${disk} --ioengine=libaio --direct=1 --size=400G ${TESTARG} --bs=${BS} --iodepth=${iodepth} --numjobs=${numjobs} --runtime=${runtime} --group_reporting --name=${disk}_${BS}_${TEST}_test --output=/dev/null
+$ENGINE $DISK $TESTARG2
 
+#if [ ! -z "${OUTPUT}" ]; then
+#	echo "Copy $OUTPUT directory to $PWD"
+#	mv ${OUTPUT} .
+#fi
